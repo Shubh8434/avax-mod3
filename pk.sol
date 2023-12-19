3e6b980bc111b7fcb8e1855eb0c38b72c6d396a8
Shubham Sharma <81500352+Shubh8434@users.noreply.github.com>
2023-12-19 09:23:07 -0500

    Create pk.sol

---
+++ pk.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PercyJackson is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("PercyJackson", "PK")
+        Ownable(initialOwner)
+    {
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
